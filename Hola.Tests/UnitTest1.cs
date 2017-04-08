e553e38a1d1945ca495734b9ad62da5b463f51fd
Angel David Galvan Peralta <Angel David Galvan Peralta>
2017-04-08 10:34:04 -0400

    Agregar archivos de proyecto.

---
+++ Hola.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hola.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var p = new Default();
+            Assert.AreEqual(2, p.suma(1, 1));
+        }
+    }
+}
---
+++ Hola/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hola
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public int suma(int a, int b)
+        {
+            return a +b;
+        }
+    }
+}
